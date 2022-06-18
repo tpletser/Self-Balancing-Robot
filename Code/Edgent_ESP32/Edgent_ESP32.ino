8b15f0a067120b77729c6d460cd801523b0f3cae
Serplet <tigran@pletser.be>
2022-06-18 23:03:10 +0200

    added kalman filter and temp sensor readings

--- Code/Edgent_ESP32/Edgent_ESP32.ino
+++ Code/Edgent_ESP32/Edgent_ESP32.ino
@@ -24,71 +24,86 @@
 #define m2PWM A16
 
 
-#include<Wire.h>
-const int MPU_addr=0x68;
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
- 
-int minVal=265;
-int maxVal=402;
- 
-double x;
-double y;
-double z;
-int32_t counter;
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+double AngleX;
+double AngleY;
+double AngleZ;
+int32_t lastMillis;
+double cycleRate;
+double sampleRate;
+double timeConstant;
+double alpha;
+double temperature;
 
 BLYNK_WRITE(V0)
 {
- int LeftMotor = param.asInt();
-  
-  if (LeftMotor >0){
-    digitalWrite(m1pin1,HIGH);
-    digitalWrite(m1pin2,LOW);
-    digitalWrite(m2pin1,HIGH);
-    digitalWrite(m2pin2,LOW);
+  int LeftMotor = param.asInt();
+
+  if (LeftMotor > 0) {
+    digitalWrite(m1pin1, HIGH);
+    digitalWrite(m1pin2, LOW);
+    digitalWrite(m2pin1, HIGH);
+    digitalWrite(m2pin2, LOW);
   }
-  else{
-    digitalWrite(m1pin1,LOW);
-    digitalWrite(m1pin2,HIGH);
-    digitalWrite(m2pin1,LOW);
-    digitalWrite(m2pin2,HIGH);
+  else {
+    digitalWrite(m1pin1, LOW);
+    digitalWrite(m1pin2, HIGH);
+    digitalWrite(m2pin1, LOW);
+    digitalWrite(m2pin2, HIGH);
   }
-  analogWrite(m1PWM,abs(LeftMotor));
-  analogWrite(m2PWM,abs(LeftMotor));
+  analogWrite(m1PWM, abs(LeftMotor));
+  analogWrite(m2PWM, abs(LeftMotor));
 }
 
 BLYNK_WRITE(V1)
 {
   int test = param.asInt();
-  digitalWrite(A15 ,test);
+  digitalWrite(A15 , test);
   Serial.print("Blynk.Cloud is writing something to V1  : ");
   Serial.println(test);
 }
 
 void setup()
 {
-  pinMode(m1pin1,OUTPUT);
-  pinMode(m1pin2,OUTPUT);
-  pinMode(m1PWM,OUTPUT);
-  pinMode(m2pin1,OUTPUT);
-  pinMode(m2pin2,OUTPUT);
-  pinMode(m2PWM,OUTPUT);
+  pinMode(m1pin1, OUTPUT);
+  pinMode(m1pin2, OUTPUT);
+  pinMode(m1PWM, OUTPUT);
+  pinMode(m2pin1, OUTPUT);
+  pinMode(m2pin2, OUTPUT);
+  pinMode(m2PWM, OUTPUT);
 
-  digitalWrite(m1pin1,LOW);
-  digitalWrite(m1pin2,LOW);
-  digitalWrite(m1PWM,LOW);
-  digitalWrite(m2pin1,LOW);
-  digitalWrite(m2pin2,LOW);
-  digitalWrite(m2PWM,LOW);
+  digitalWrite(m1pin1, LOW);
+  digitalWrite(m1pin2, LOW);
+  digitalWrite(m1PWM, LOW);
+  digitalWrite(m2pin1, LOW);
+  digitalWrite(m2pin2, LOW);
+  digitalWrite(m2PWM, LOW);
 
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-  counter = 0;
-  
   Serial.begin(115200);
   delay(100);
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  lastMillis = 0;
+  sampleRate = 100; // in ms
+  AngleX = 0;
+  AngleY = 0;
+  AngleZ = 0;
+  timeConstant = 1;
+  alpha = timeConstant / (timeConstant + sampleRate / 1000) ;
+
 
   BlynkEdgent.begin();
 }
@@ -96,36 +111,54 @@ void setup()
 void loop() {
   BlynkEdgent.run();
 
-  if(counter>1000){
-    counter =0;
-    Wire.beginTransmission(MPU_addr);
-    Wire.write(0x3B);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU_addr,14,true);
-    AcX=Wire.read()<<8|Wire.read();
-    AcY=Wire.read()<<8|Wire.read();
-    AcZ=Wire.read()<<8|Wire.read();
-    int xAng = map(AcX,minVal,maxVal,-90,90);
-    int yAng = map(AcY,minVal,maxVal,-90,90);
-    int zAng = map(AcZ,minVal,maxVal,-90,90);
-     
-    x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
-    y= RAD_TO_DEG * (atan2(-xAng, -zAng));
-    z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
-     
-    Serial.print("AngleX= ");
-    Serial.println(x);
-     
-    Serial.print("AngleY= ");
-    Serial.println(y);
-     
-    Serial.print("AngleZ= ");
-    Serial.println(z);
-    Serial.println("-----------------------------------------");
-     
-    //Blynk.virtualWrite(V2, x);
-    Blynk.virtualWrite(V2, y);
-    //Blynk.virtualWrite(V4, z);
-  } 
-  counter++;
+  if (millis() - lastMillis > sampleRate) {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    temperature = temp.temperature;
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+
+    double AGx, AGy, AGz;
+    AGx = AngleX + RAD_TO_DEG * g.gyro.x * sampleRate / 1000;
+    AGy = AngleY - RAD_TO_DEG * g.gyro.y * sampleRate / 1000;
+    AGz = AngleZ + RAD_TO_DEG * g.gyro.z * sampleRate / 1000;
+
+    Serial.print("Gyro X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad");
+    double AAccx, AAccy, AAccz;
+    AAccx = RAD_TO_DEG * (atan2(-a.acceleration.y, -a.acceleration.z));
+    AAccy = RAD_TO_DEG * (atan2(-a.acceleration.x, -a.acceleration.z));
+    AAccz = RAD_TO_DEG * (atan2(-a.acceleration.y, -a.acceleration.x));
+    Serial.print("Acc angle X: ");
+    Serial.print(AAccx);
+    Serial.print(", Y: ");
+    Serial.print(AAccy);
+    Serial.print(", Z: ");
+    Serial.print(AAccz);
+    Serial.println(" deg");
+
+    AngleX = alpha * AGx + (1 - alpha) * AAccx;
+    AngleY = alpha * AGy + (1 - alpha) * AAccy;
+    AngleZ = alpha * AGz + (1 - alpha) * AAccz;
+    Serial.print("Comp angle X: ");
+    Serial.print(AngleX);
+    Serial.print(", Y: ");
+    Serial.print(AngleY);
+    Serial.print(", Z: ");
+    Serial.print(AngleZ);
+    Serial.println(" deg");
+    
+  }
+  if (millis() - lastMillis > 500) {//augmenter delay quand c'est controllé
+    Blynk.virtualWrite(V2, AngleY);
+  }
+  if (millis() - lastMillis > 2000) {
+    Blynk.virtualWrite(V6, temperature);
+  }
 }
